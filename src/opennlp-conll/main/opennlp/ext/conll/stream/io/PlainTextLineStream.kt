1ebbd652ffb920859581d6cadff20b955d75cbf6
Reece H. Dunn <msclrhd@gmail.com>
2021-10-15 20:58:42 +0100

    Support loading a PlainTextLineStream from a file or path.

--- src/opennlp-conll/main/opennlp/ext/conll/stream/io/PlainTextLineStream.kt
+++ src/opennlp-conll/main/opennlp/ext/conll/stream/io/PlainTextLineStream.kt
@@ -2,13 +2,23 @@
 package opennlp.ext.conll.stream.io
 
 import opennlp.tools.util.InputStreamFactory
+import opennlp.tools.util.MarkableFileInputStreamFactory
 import opennlp.tools.util.ObjectStream
 import opennlp.tools.util.PlainTextByLineStream
+import java.io.File
 import java.nio.charset.Charset
 import java.nio.charset.StandardCharsets
 
 object PlainTextLineStream {
     fun create(stream: InputStreamFactory, charset: Charset = StandardCharsets.UTF_8): ObjectStream<String> {
         return PlainTextByLineStream(stream, charset)
+    }
+
+    fun load(file: File, charset: Charset = StandardCharsets.UTF_8): ObjectStream<String> {
+        return create(MarkableFileInputStreamFactory(file), charset)
+    }
+
+    fun load(path: String, charset: Charset = StandardCharsets.UTF_8): ObjectStream<String> {
+        return load(File(path), charset)
     }
 }
