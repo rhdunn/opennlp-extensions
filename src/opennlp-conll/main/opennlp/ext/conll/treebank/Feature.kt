84d78dcb3c056da668cabea1977b32868c57ae16
Reece H. Dunn <msclrhd@gmail.com>
2021-10-15 19:56:08 +0100

    Parse a feature list.

--- src/opennlp-conll/main/opennlp/ext/conll/treebank/Feature.kt
+++ src/opennlp-conll/main/opennlp/ext/conll/treebank/Feature.kt
@@ -8,4 +8,27 @@ data class Feature(val name: String?, val value: String) {
         null -> value
         else -> "$name=$value"
     }
+
+    class MissingFeatureException: RuntimeException("Missing feature")
+
+    class InvalidFeatureException(feat: String): RuntimeException("Invalid feature: $feat")
+
+    companion object {
+        private fun parseItem(feat: String): Feature = when (feat) {
+            "" -> throw MissingFeatureException()
+            else -> {
+                val parts = feat.split('=')
+                when (parts.size) {
+                    1 -> Feature(null, parts[0])
+                    2 -> Feature(parts[0], parts[1])
+                    else -> throw InvalidFeatureException(feat)
+                }
+            }
+        }
+
+        fun parse(feats: String): List<Feature> = when (feats) {
+            "_" -> listOf()
+            else -> feats.split('|').map { parseItem(it) }
+        }
+    }
 }
--- src/opennlp-conll/test/opennlp/ext/conll/tests/treebank/FeatureTest.kt
+++ src/opennlp-conll/test/opennlp/ext/conll/tests/treebank/FeatureTest.kt
@@ -5,7 +5,9 @@ import opennlp.ext.conll.treebank.Feature
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 @DisplayName("CoNLL Feature")
 class FeatureTest {
@@ -21,5 +23,52 @@ class FeatureTest {
     fun named() {
         val feat = Feature("lorem", "ipsum")
         assertThat(feat.toString(), `is`("lorem=ipsum"))
+    }
+
+    @Nested
+    @DisplayName("parse list")
+    inner class ParseList {
+        @Test
+        @DisplayName("empty")
+        fun empty() {
+            val feat = Feature.parse("_")
+            assertThat(feat.size, `is`(0))
+        }
+
+        @Test
+        @DisplayName("invalid: missing value")
+        fun missingValue() {
+            val e = assertThrows<Feature.MissingFeatureException> {
+                Feature.parse("")
+            }
+            assertThat(e.message, `is`("Missing feature"))
+        }
+
+        @Test
+        @DisplayName("one")
+        fun one() {
+            val feat = Feature.parse("one=lorem")
+            assertThat(feat[0], `is`(Feature("one", "lorem")))
+            assertThat(feat.size, `is`(1))
+        }
+
+        @Test
+        @DisplayName("multiple")
+        fun multiple() {
+            val feat = Feature.parse("one=lorem|ipsum|three=dolor")
+            assertThat(feat[0], `is`(Feature("one", "lorem")))
+            assertThat(feat[1], `is`(Feature(null, "ipsum")))
+            assertThat(feat[2], `is`(Feature("three", "dolor")))
+            assertThat(feat.size, `is`(3))
+        }
+
+        @Test
+        @DisplayName("invalid: too many parts")
+        fun tooManyParts() {
+            val e = assertThrows<Feature.InvalidFeatureException> {
+                Feature.parse("one=lorem=ipsum")
+            }
+            assertThat(e.message, `is`("Invalid feature: one=lorem=ipsum"))
+        }
     }
 }
