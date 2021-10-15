31d326f86f4ea34ad06355109d928dd753e81b43
Reece H. Dunn <msclrhd@gmail.com>
2021-10-15 21:11:23 +0100

    Support creating a PlainTextLineStream from a byte array or string.

---
+++ src/opennlp-conll/main/opennlp/ext/conll/stream/io/ByteArrayInputStreamFactory.kt
@@ -0,0 +1,14 @@
+// Copyright (C) 2021 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
+package opennlp.ext.conll.stream.io
+
+import opennlp.tools.util.InputStreamFactory
+import java.io.ByteArrayInputStream
+import java.io.InputStream
+import java.nio.charset.Charset
+import java.nio.charset.StandardCharsets
+
+class ByteArrayInputStreamFactory(private val bytes: ByteArray) : InputStreamFactory {
+    constructor(string: String, charset: Charset = StandardCharsets.UTF_8) : this(string.toByteArray(charset))
+
+    override fun createInputStream(): InputStream = ByteArrayInputStream(bytes)
+}
--- src/opennlp-conll/main/opennlp/ext/conll/stream/io/PlainTextLineStream.kt
+++ src/opennlp-conll/main/opennlp/ext/conll/stream/io/PlainTextLineStream.kt
@@ -14,6 +14,14 @@ object PlainTextLineStream {
         return PlainTextByLineStream(stream, charset)
     }
 
+    fun create(bytes: ByteArray, charset: Charset = StandardCharsets.UTF_8): ObjectStream<String> {
+        return create(ByteArrayInputStreamFactory(bytes), charset)
+    }
+
+    fun create(string: String): ObjectStream<String> {
+        return create(ByteArrayInputStreamFactory(string))
+    }
+
     fun load(file: File, charset: Charset = StandardCharsets.UTF_8): ObjectStream<String> {
         return create(MarkableFileInputStreamFactory(file), charset)
     }
