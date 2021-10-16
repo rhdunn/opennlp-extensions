5a2106ff8e00a76122596fb92f7ce7cee9588b29
Reece H. Dunn <msclrhd@gmail.com>
2021-10-16 12:37:16 +0100

    Add helpers to construct a sentence stream that mirror the PlainTextLineStream helpers.

--- src/opennlp-conll/main/opennlp/ext/conll/stream/sentence/SentenceStream.kt
+++ src/opennlp-conll/main/opennlp/ext/conll/stream/sentence/SentenceStream.kt
@@ -1,8 +1,11 @@
 // Copyright (C) 2021 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
 package opennlp.ext.conll.stream.sentence
 
+import opennlp.ext.conll.stream.io.PlainTextLineStream
 import opennlp.ext.conll.treebank.Sentence
+import opennlp.tools.util.InputStreamFactory
 import opennlp.tools.util.ObjectStream
+import java.io.File
 
 @Suppress("MemberVisibilityCanBePrivate")
 object SentenceStream {
@@ -16,5 +19,29 @@ object SentenceStream {
         CONLLX -> ConllxSentenceStream(stream)
         CONLLU -> ConlluSentenceStream(stream)
         else -> throw UnsupportedSentenceFormatException(format)
+    }
+
+    fun create(stream: InputStreamFactory, format: String): ObjectStream<Sentence> {
+        val lines = PlainTextLineStream.create(stream)
+        return create(lines, format)
+    }
+
+    fun create(bytes: ByteArray, format: String): ObjectStream<Sentence> {
+        val lines = PlainTextLineStream.create(bytes)
+        return create(lines, format)
+    }
+
+    fun create(string: String, format: String): ObjectStream<Sentence> {
+        val lines = PlainTextLineStream.create(string)
+        return create(lines, format)
+    }
+
+    fun load(file: File): ObjectStream<Sentence> {
+        val lines = PlainTextLineStream.load(file)
+        return create(lines, file.extension)
+    }
+
+    fun load(path: String): ObjectStream<Sentence> {
+        return load(File(path))
     }
 }
