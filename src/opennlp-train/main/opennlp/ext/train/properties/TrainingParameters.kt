0bcc0135d2b1edb74c03c29a5147d20a63fe3d47
Reece H. Dunn <msclrhd@gmail.com>
2021-10-16 16:58:47 +0100

    Add a helper for creating TrainingParameters from a properties file.

---
+++ src/opennlp-train/main/opennlp/ext/train/properties/TrainingParameters.kt
@@ -0,0 +1,10 @@
+// Copyright (C) 2021 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
+package opennlp.ext.train.properties
+
+import opennlp.tools.util.TrainingParameters
+import java.util.*
+
+fun trainingParameters(properties: Properties): TrainingParameters {
+    val params = TrainingParameters()
+    return params
+}
