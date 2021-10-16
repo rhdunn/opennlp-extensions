9ec0698e6aaa16d82747f72de18d10240234a1d2
Reece H. Dunn <msclrhd@gmail.com>
2021-10-16 17:52:31 +0100

    Support resolving a path-based property in a Properties file.

--- src/opennlp-train/main/opennlp/ext/train/properties/Properties.kt
+++ src/opennlp-train/main/opennlp/ext/train/properties/Properties.kt
@@ -1,6 +1,7 @@
 // Copyright (C) 2021 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
 package opennlp.ext.train.properties
 
+import java.io.File
 import java.io.FileReader
 import java.util.*
 
@@ -8,6 +9,15 @@ fun properties(path: String?): Properties {
     val properties = Properties()
     path?.let {
         properties.load(FileReader(it))
+        properties[BASE_PATH] = File(path).parentFile
     }
     return properties
+}
+
+private const val BASE_PATH = "properties.base.path" // file [internal]
+
+fun path(properties: Properties, key: String): File? {
+    val path = properties[key] as? String ?: return null
+    val base = properties[BASE_PATH] as File
+    return base.resolve(path)
 }
