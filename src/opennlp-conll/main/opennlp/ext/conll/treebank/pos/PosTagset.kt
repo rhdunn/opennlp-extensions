1b38402f670597fa9fbc077be19a2eb47b5d79a0
Reece H. Dunn <msclrhd@gmail.com>
2021-10-21 20:37:37 +0100

    Support selecting a POS tagset by name.

--- src/opennlp-conll/main/opennlp/ext/conll/treebank/pos/PosTagset.kt
+++ src/opennlp-conll/main/opennlp/ext/conll/treebank/pos/PosTagset.kt
@@ -1,6 +1,19 @@
 // Copyright (C) 2021 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
 package opennlp.ext.conll.treebank.pos
 
+import opennlp.ext.conll.treebank.pos.tags.UPennTags
+import opennlp.ext.conll.treebank.pos.tags.UPosTags
+
 interface PosTagset {
     operator fun get(tag: String): PosTag?
+
+    class UnsupportedTagsetException(tagset: String) : RuntimeException("Unsupported POS tagset: $tagset")
+
+    companion object {
+        fun create(tagset: String): PosTagset = when (tagset) {
+            "upos" -> UPosTags
+            "upenn" -> UPennTags
+            else -> throw UnsupportedTagsetException(tagset)
+        }
+    }
 }
