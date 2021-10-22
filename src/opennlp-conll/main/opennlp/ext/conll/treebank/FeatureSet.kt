c3a827a643c21e42bc8ea48655d16ced873c7fae
Reece H. Dunn <msclrhd@gmail.com>
2021-10-22 15:44:18 +0100

    Add a FeatureSet#get helper function.

--- src/opennlp-conll/main/opennlp/ext/conll/treebank/FeatureSet.kt
+++ src/opennlp-conll/main/opennlp/ext/conll/treebank/FeatureSet.kt
@@ -1,8 +1,17 @@
 // Copyright (C) 2021 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
 package opennlp.ext.conll.treebank
 
+import opennlp.ext.conll.treebank.features.misc.SpaceAfter
+
 interface FeatureSet {
     val type: String
 
     fun create(value: String): Feature
+
+    companion object {
+        operator fun get(type: String): FeatureSet? = when (type) {
+            SpaceAfter.type -> SpaceAfter
+            else -> null
+        }
+    }
 }
