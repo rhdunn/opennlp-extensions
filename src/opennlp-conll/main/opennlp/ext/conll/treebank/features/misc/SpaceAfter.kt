0a5fe338f1a213ec7d61ab68cc4bac9b6e3eb558
Reece H. Dunn <msclrhd@gmail.com>
2021-10-22 15:38:10 +0100

    Add a SpaceAfter#create helper function.

--- src/opennlp-conll/main/opennlp/ext/conll/treebank/features/misc/SpaceAfter.kt
+++ src/opennlp-conll/main/opennlp/ext/conll/treebank/features/misc/SpaceAfter.kt
@@ -2,6 +2,7 @@
 package opennlp.ext.conll.treebank.features.misc
 
 import opennlp.ext.conll.treebank.Feature
+import opennlp.ext.conll.treebank.features.UnknownFeatureValue
 
 // Reference: [CoNLL-U Format](https://universaldependencies.org/format.html)
 enum class SpaceAfter : Feature {
@@ -11,4 +12,10 @@ enum class SpaceAfter : Feature {
     override val value: String = name
 
     override fun toString(): String = "$type=$value"
+
+    companion object {
+        fun create(value: String): Feature {
+            return values().find { it.value == value } ?: UnknownFeatureValue("SpaceAfter", value)
+        }
+    }
 }
