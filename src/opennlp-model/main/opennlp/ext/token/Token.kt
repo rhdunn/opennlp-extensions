24070c14bc87756f7ee61f416a785f8417762d60
Reece H. Dunn <msclrhd@gmail.com>
2021-10-24 09:35:10 +0100

    Create a Token#tokenizer helper function.

---
+++ src/opennlp-model/main/opennlp/ext/token/Token.kt
@@ -0,0 +1,30 @@
+// Copyright (C) 2021 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
+package opennlp.ext.token
+
+import opennlp.tools.tokenize.Tokenizer
+import opennlp.tools.tokenize.TokenizerME
+import opennlp.tools.tokenize.TokenizerModel
+import java.io.File
+import java.io.InputStream
+import java.net.URL
+import java.nio.file.Path
+
+@Suppress("MemberVisibilityCanBePrivate")
+class Token {
+    companion object {
+        fun tokenizer(model: TokenizerModel): Tokenizer = TokenizerME(model)
+
+        fun tokenizer(stream: InputStream): Tokenizer = tokenizer(TokenizerModel(stream))
+
+        fun tokenizer(file: File): Tokenizer = tokenizer(TokenizerModel(file))
+
+        fun tokenizer(path: Path): Tokenizer = tokenizer(TokenizerModel(path))
+
+        fun tokenizer(url: URL): Tokenizer = tokenizer(TokenizerModel(url))
+
+        fun tokenizer(path: String): Tokenizer = when {
+            path.contains("://") -> tokenizer(URL(path))
+            else -> tokenizer(File(path))
+        }
+    }
+}
